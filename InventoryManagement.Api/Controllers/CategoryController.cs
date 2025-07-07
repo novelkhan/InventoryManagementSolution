686b398fcb2a3eb7865232eae14e8e27fb08b2dc
novelkhan <novel4004@gmail.com>
2025-07-07 17:06:48 +0600

    https://grok.com/chat/e7ee4e6b-6280-46c7-8fca-5675a6f23bae

---
+++ InventoryManagement.Api/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using InventoryManagement.Common.Dtos;
+using InventoryManagement.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllCategories();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CategoryDto categoryDto)
+        {
+            await _categoryService.AddCategory(categoryDto);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] CategoryDto categoryDto)
+        {
+            await _categoryService.UpdateCategory(id, categoryDto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.DeleteCategory(id);
+            return Ok();
+        }
+    }
+}
